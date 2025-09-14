507ea8ec39a9eb295414a51c1a3f01ef1ebc028e
Yuseon Kim(썬데이) <74897720+useon@users.noreply.github.com>
2025-09-14 15:07:08 +0900

    [Silver IV] Title: 귀찮아 (SIB), Time: 180 ms, Memory: 17576 KB -BaekjoonHub

--- 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.js
+++ 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.js
@@ -2,26 +2,20 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 const N = Number(input[0]);
-const X = input[1].split(' ').map(Number);
-const prefixSum = new Array(X.length);
-prefixSum[0] = X[0]; // 첫 번째 요소의 누적합은 자기 자신이다
+const info = input[1].split(' ').map(Number);
 
-// 누적합 만들기
-for(let i = 1; i < X.length; i++) {
-    prefixSum[i] = prefixSum[i - 1] + X[i];
+// 누적합
+const prefixSum = Array(N);
+// 누적합의 첫번째는 자기자신
+prefixSum[0] = info[0];
+
+for(let i = 1; i < N; i++) {
+    prefixSum[i] = prefixSum[i - 1] + info[i];
 }
 
-// 구간합 구하기
-// 하나하나 구하려고 하면 시간초과의 가능성이 있음
-// 따라서 (전체 합의 제곱 - 각 원소 제곱의 합) / 2
-// O(N)으로 계산할 수 있음
+// 구간합 
 let sum = 0;
-let sumSquares = 0;
-
-for(let num of X) {
-    sum += num;
-    sumSquares += num * num;
+for(let j = 1; j < N; j++) {
+    sum += info[j] * prefixSum[j - 1];
 }
-
-const result = (sum * sum - sumSquares) / 2;
-console.log(result);
+console.log(sum);
