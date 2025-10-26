dafbac6e5d86c60fa348d156c4c66ea1c0f4d0bb
Yuseon Kim(썬데이) <74897720+useon@users.noreply.github.com>
2025-10-27 00:14:04 +0900

    [Gold V] Title: 공주님을 구해라!, Time: 192 ms, Memory: 15860 KB -BaekjoonHub

--- 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.js
+++ 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.js
@@ -1,55 +1,53 @@
 const fs = require('fs');
-const filePath =
-  process.platform === 'linux' ? '/dev/stdin' : '../../input.txt';
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
-// 크기 N, M 제한시간 T
-const [N, M, T] = input[0].split(' ').map(Number);
-// 공주에게 도달할 수 있는 최단 시간을 출력
-const info = input.slice(1).map((x) => x.split(' ').map(Number));
-const dx = [-1, 1, 0, 0];
-const dy = [0, 0, -1, 1];
+const [n, m, t] = input[0].split(' ').map(Number);
+const graph = [Array(n+1).fill(0)]
+const dRow = [-1, 1, 0, 0]
+const dCol = [0, 0, -1, 1]
 
-const result = bfs(0, 0);
-console.log(result);
+for(let i = 1; i <= n; i++) {
+    const line = [0, ...input[i].split(' ').map(Number)]
+    graph.push(line)
+}
 
-// 시작 x, y 좌표를 통해 ~ N, M까지 가는 시간을 구해야 한다.
-function bfs(x, y) {
-    const queue = [[0, 0, 0, 0]];
-    // visited에 그람 없는 방문, 그람 있는 방문을 가지고 있다.
-    const visited = Array.from({ length: N }, () => 
-        Array.from({ length: M }, () => Array(2).fill(false))
-    );    
-    visited[0][0][0] = true;
-    
-    while(queue.length > 0) {
-        const [curX, curY, hasGram, time] = queue.shift();
+function bfs(startRow, startCol) {
+    const queue = []
+    let index = 0
+    let shortestTime = Infinity
+    const visited = Array.from({length: n + 1}, () => Array.from({length: m + 1}, () => [false, false]))
+    queue.push([startRow, startCol, 0, false])
+    visited[startRow][startCol][0] = true
 
-        if(curX === N - 1 && curY === M - 1) {
-            return time > T ? 'Fail' : time;
+    while(queue.length > index) {
+        const [curRow, curCol, curTime, hasGram] = queue[index]
+
+        if(curRow === n && curCol === m) {
+            shortestTime = Math.min(shortestTime, curTime)
         }
-        
-        for(let i = 0; i < 4; i++) {
-            const nx = curX + dx[i];
-            const ny = curY + dy[i];
 
-            if(nx >= 0 && nx < N && ny >= 0 && ny < M) {
-                // 그람을 만나면 넣어준다.
-                if(!visited[nx][ny][hasGram] && !hasGram && info[nx][ny] === 2) {
-                    visited[nx][ny][hasGram] = true;
-                    queue.push([nx, ny, true, time + 1]);
-                }
-                // 벽이고 그람을 가지고 있는 경우는 벽을 부순다.
-                if(!visited[nx][ny][hasGram] && info[nx][ny] === 1 && hasGram) {
-                    visited[nx][ny][hasGram] = true;
-                    queue.push([nx, ny, hasGram, time + 1]);
-                }
-                // 벽이 아닌 경우
-                if(!visited[nx][ny][hasGram] && info[nx][ny] === 0) {
-                    visited[nx][ny][hasGram] = true;
-                    queue.push([nx, ny, hasGram, time + 1]);
+        for(let i = 0; i < 4; i++) {
+            const nextRow = curRow + dRow[i]
+            const nextCol = curCol + dCol[i]
+            const nextTime = curTime + 1
+
+            // 범위와 방문 여부 체크
+            if(nextRow > 0 && nextRow <= n && nextCol > 0 && nextCol <= m) {
+                // gram 여부에 따라서 가능한 경로가 달라짐
+                // 지금 gram을 가지고 있거나 다음 자리에서 가지게 된다면 다음 위치를 queue에 넣어준다.
+                if((hasGram || graph[nextRow][nextCol] === 2) && !visited[nextRow][nextCol][1]) {
+                    queue.push([nextRow, nextCol, nextTime, true])
+                    visited[nextRow][nextCol][1] = true
+                } else if(!hasGram && graph[nextRow][nextCol] == 0 && !visited[nextRow][nextCol][0]) {
+                    queue.push([nextRow, nextCol, nextTime, false])
+                    visited[nextRow][nextCol][0] = true
                 }
             }
         }
+        index += 1
     }
-    return 'Fail'
+    return shortestTime
 }
+
+const arrivedTime = bfs(1, 1)
+arrivedTime > t ? console.log('Fail') : console.log(arrivedTime)
