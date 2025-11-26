512a4f1cdf306bb883938fdc9d1bbdfbe6095a01
Yuseon Kim(썬데이) <74897720+useon@users.noreply.github.com>
2025-11-27 04:09:17 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 100 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.js
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.js
@@ -1,18 +1,16 @@
 const fs = require('fs');
-const filePath =
-  process.platform === 'linux' ? '/dev/stdin' : '../../input.txt';
-const input = fs.readFileSync(filePath).toString().trim().split('\n');
-const N = Number(input);
+const input = fs.readFileSync(0, 'utf-8').trim().split('\n');
+const n = Number(input[0])
 
-const fib = (n) => {
-  const dp = Array(n + 1).fill(0);
-  dp[1] = 1;
-
-  for (let i = 2; i <= n; i++) {
-    dp[i] = BigInt(dp[i - 1]) + BigInt(dp[i - 2]);
-  }
+function fib(n) {
+    const dp = Array(n + 1).fill(0)
+    dp[1] = 1
+    
+    for (let i = 2; i <= n; i++) {
+        dp[i] = BigInt(dp[i - 1]) + BigInt(dp[i - 2])    
+    }
 
   return dp[n];
-};
+}
 
-console.log(fib(N).toString());
+console.log(fib(n).toString());
