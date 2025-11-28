185883d165693308167d63774dc4b47ed9af9cb3
Yuseon Kim(썬데이) <74897720+useon@users.noreply.github.com>
2025-11-28 23:48:23 +0900

    [Silver V] Title: 돌 게임, Time: 148 ms, Memory: 9380 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.js
+++ 백준/Silver/9655. 돌 게임/돌 게임.js
@@ -1,12 +1,9 @@
 const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : '../../input.txt';
-const input = fs.readFileSync(filePath).toString().trim();
-// 돌 N개
-// 돌 1개 또는 3개 가져갈 수 있다.
-// 마지막 돌을 가져가는 사람이 승
-const N = Number(input);
-// 상근이가 먼저 시작함
-// 1 또 는 3을 가져갈 수 있는데 이기는 사람 어떻게 판단해?
-// 어차피 N이 홀수이면 시작하는 사람이 마지막 - 상근
-// 짝수이면 다른 사람이 마지막. - 창영
-console.log(N % 2 === 0 ? 'CY' : 'SK');
+const input = fs.readFileSync(0, 'utf-8').trim().split('\n');
+const n = Number(input[0])
+
+if(n % 2 === 0) {
+    console.log('CY')
+} else {
+    console.log('SK')
+}
