9d457f016cb1dcc5690d26f4e803f476c5dbfc94
Yuseon Kim(썬데이) <74897720+useon@users.noreply.github.com>
2025-12-04 03:11:18 +0900

    [Silver III] Title: 1로 만들기, Time: 164 ms, Memory: 17452 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.js
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -2,7 +2,8 @@ const fs = require('fs');
 const filePath =
   process.platform === 'linux' ? '/dev/stdin' : '../../input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
-const N = Number(input);
+
+const N = Number(input[0]);
 const dp = new Array(N + 1).fill(0);
 
 for (let i = 2; i <= N; i++) {
